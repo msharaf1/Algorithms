7cec8fbe4d599e6ab0b8edef428e34ed264fb3a8
msharaf1 <menhaj0102@gmail.com>
2018-11-01 19:14:24 -0400

    commit

--- CodingDojoPt.js
+++ CodingDojoPt.js
@@ -137,14 +137,50 @@
 
 // Greater than y;
 
-let x = [1,2,3,4,5,6,7,8,9,10,12,14,15,16,17,18,256];
-let y = 3;
-function greaterThanY(x,y){
-    let gY = [];
-    for(let i =0; i < x.length; i++){
-        if(y < x[i]){
-            gY.push(x[i]);
+// let x = [1,2,3,4,5,6,7,8,9,10,12,14,15,16,17,18,256];
+// let y = 3;
+// function greaterThanY(x,y){
+//     let gY = [];
+//     for(let i =0; i < x.length; i++){
+//         if(y < x[i]){
+//             gY.push(x[i]);
+//         }
+//     } console.log(gY);
+// }
+
+// greaterThanY(x, y);
+// let x = [1,5, 10, -2];
+// function sqrValue(x){
+//     let sqrVal = [];
+//     for(let i=0; i<x.length; i++){
+//         sqrVal.push(x[i] * x[i]);
+//     }
+//     console.log(sqrVal);
+// };
+// sqrValue(x);
+
+
+// let x = [1,5, 10, -2];
+// function sqrValue(x){
+//     let sqrVal = [];
+//     for(let i=0; i<x.length; i++){
+//         if(sqrVal[0] ===null){
+//             sqrVal[0] = x[i] * x[i];
+//         }else if(sqrVal[0] !==null){
+//             sqrVal[i] = x[i] * x[i];
+//         }
+//     }
+//     console.log(sqrVal);
+// };
+// sqrValue(x);
+
+let x =  [1,5, 10, -2];
+function eliminateNeg(x){
+    for(let i =0; i< x.length; i++){
+        if(x[i] <0){
+            x[i] = 0;
         }
-    } console.log(gY);
+    }
+    console.log(x);
 }
-greaterThanY(x, y);
+eliminateNeg(x);
