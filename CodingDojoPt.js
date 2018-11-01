6455875e4acdcce97ac9e6ef4e985311ba089e8f
msharaf1 <menhaj0102@gmail.com>
2018-11-01 12:02:56 -0400

    commit

--- CodingDojoPt.js
+++ CodingDojoPt.js
@@ -1,8 +1,20 @@
 // Print 1-255
 
-function print255(){
-    for(let i=0; i<=255; i++){
-        console.log(i);
-    }
-}
-console.log(print255());
+// function print255(){
+//     for(let i=0; i<=255; i++){
+//         console.log(i);
+//     }
+// }
+// console.log(print255());
+
+//Print Odd Numbers;
+
+// function printOdd1000(){
+//     for(let i=0; i<= 1000; i++){
+//         console.log(i);
+//     }
+// }
+
+// printOdd1000();
+
+
